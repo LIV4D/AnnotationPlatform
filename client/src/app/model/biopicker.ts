7803d65825751d54680312b126eb2b36d5b48bf9
Gabriel Lepetit-Aimon <gabriel.lepetitaimon@gmail.com>
2018-12-20 00:20:03 +0100

    Commit fix

---
+++ client/src/app/model/biopicker.ts
@@ -0,0 +1,28 @@
+import { Tool } from './tool';
+import { Point } from './point';
+
+export class BioPicker extends Tool {
+
+    constructor(name: string, iconPath: string, tooltip: string) {
+        super(name, iconPath, tooltip);
+    }
+
+    onCursorDown(point: Point): void {
+        this.selectUnder(point);
+    }
+
+    selectUnder(point: Point): boolean {
+        const biomarkers = this.layersService.getBiomarkerCanvas();
+        for (let i = 0; i < biomarkers.length; i++) {
+            const b = biomarkers[i];
+            const b_id = b.id.substr(11); // HACK TO REMOVE annotation_ prefix
+            const data = b.displayCanvas.getContext('2d').getImageData(point.x, point.y, 1, 1);
+            if (data.data[3] > 120) {
+                const biomarkerElem = this.biomarkersService.flat.filter((e) => e.id === b_id)[0];
+                this.biomarkersService.setFocusBiomarker(biomarkerElem);
+                return true;
+            }
+        }
+        return false;
+    }
+}
