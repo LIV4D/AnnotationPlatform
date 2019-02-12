1f72afe5cda48b716425a95086c6ac4166c06017
Gabriel Lepetit-Aimon <gabriel.lepetitaimon@gmail.com>
2019-02-11 20:40:49 -0500

    Bug fix

--- client/src/app/model/biopicker.ts
+++ client/src/app/model/biopicker.ts
@@ -13,10 +13,22 @@ export class BioPicker extends Tool {
 
     selectUnder(point: Point): boolean {
         const biomarkers = this.layersService.getBiomarkerCanvas();
+
+        // HACK... 2 offset, wtf?
+        let correctionX = 0, correctionY = 0;
+        if (biomarkers[0].currentCanvas.width < this.layersService.biomarkerOverlayCanvas.width) {
+            correctionX = (biomarkers[0].currentCanvas.width - this.layersService.biomarkerOverlayCanvas.width) / 2;
+        }
+        if (biomarkers[0].currentCanvas.height < this.layersService.biomarkerOverlayCanvas.height) {
+            correctionY = (biomarkers[0].currentCanvas.height - this.layersService.biomarkerOverlayCanvas.height) / 2;
+        }
+        point.x = point.x + biomarkers[0].offsetX + correctionX;
+        point.y = point.y + biomarkers[0].offsetY + correctionY;
+
         for (let i = 0; i < biomarkers.length; i++) {
             const b = biomarkers[i];
             const b_id = b.id.substr(11); // HACK TO REMOVE annotation_ prefix
-            const data = b.currentCanvas.getContext('2d').getImageData(b.offsetX + point.x, b.offsetY + point.y, 1, 1);
+            const data = b.currentCanvas.getContext('2d').getImageData(point.x, point.y, 1, 1);
             if (data.data[3] > 120) {
                 const biomarkerElem = this.biomarkersService.flat.filter((e) => e.id === b_id)[0];
                 this.biomarkersService.setFocusBiomarker(biomarkerElem);
