c311de8009d7cf590515f380675a551583e36b8f
Monssaf Toukal <monsasf.toukal@polymtl.ca>
2019-05-22 14:08:51 -0400

    Changing models for relations : User, Evenement

---
+++ server/app/models/evenement.model.ts
@@ -0,0 +1,6 @@
+import { Column, Entity, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+import {User} from './user.model'
+
+@Entity()
+export class Evenement {
+}
--- server/app/models/user.model.ts
+++ server/app/models/user.model.ts
@@ -1,8 +1,8 @@
 import 'reflect-metadata';
 import * as crypto from 'crypto';
-import { IsEmail, IsEnum } from 'class-validator';
-import { Column, Entity, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
-import { Revision } from './revision.model';
+import { IsEmail } from 'class-validator';
+import { Column, Entity, OneToMany, PrimaryColumn } from 'typeorm';
+import { Evenement } from './evenement.model'
 import { Task } from './task.model';
 import { isUndefined } from 'util';
 
@@ -13,35 +13,40 @@ export enum UserRole {
 
 @Entity()
 export class User {
-    @PrimaryGeneratedColumn('uuid')
+    @PrimaryColumn({ length: 16 })
     public id: string;
 
-    @OneToMany(type => Revision, revision => revision.user)
-    public revisions: Revision[];
+    // @OneToMany(type => Revision, revision => revision.user)
+    // public revisions: Revision[];
+
+    @OneToMany(type => Evenement, evenement => evenement.user)
+    public evenements: Evenement[];
 
     @OneToMany(type => Task, task => task.user)
     public tasks: Task[];
 
-    @Column({ length : 32 })
-    public name: string;
+    @Column({ length: 32 })
+    public firstName: string;
+
+    @Column({ length: 32 })
+    public lastName: string;
 
     @Column({ length: 254, unique: true })
     @IsEmail()
     public email: string;
 
-    @Column({ type: 'bytea', select: false })
-    public hash: Buffer;
+    @Column({ select: false })
+    public hash: string;
 
-    @Column({ type: 'bytea', select: false })
-    public salt: Buffer;
+    @Column({ select: false })
+    public salt: string;
 
-    @Column()
-    @IsEnum(UserRole)
-    role: UserRole;
+    @Column({ default : false })
+    isAdmin: boolean;
 
-    public static hashPassword(password: string, salt?: Buffer) {
+    public static hashPassword(password: string, salt?: string) {
         if (isUndefined(salt)) {
-            salt = crypto.randomBytes(64);
+            salt = crypto.randomBytes(64).toString();
         }
         return {
             hash: crypto.pbkdf2Sync(password, salt, 20000, 64, 'sha512'),
@@ -51,6 +56,6 @@ export class User {
 
     public hashCompare(passwordProvided: string) {
         const hashProvided = User.hashPassword(passwordProvided, this.salt).hash;
-        return crypto.timingSafeEqual(this.hash, hashProvided);
+        return crypto.timingSafeEqual(new Buffer(this.hash), hashProvided);
     }
 }
