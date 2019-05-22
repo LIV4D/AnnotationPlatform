ea1ef89e4272091ccb2a329cbf34761d7aeee9f4
Monssaf Toukal <monsasf.toukal@polymtl.ca>
2019-05-22 16:46:00 -0400

    changement de task prototype

--- server/app/models/taskPrototype.model.ts
+++ server/app/models/taskPrototype.model.ts
@@ -1,4 +1,4 @@
-import { TaskType } from './taskType.model';
+import { TaskGroup } from './taskType.model';
 import { User } from './user.model';
 import { ImagePrototype } from './imagePrototype.model';
 import { Task } from './task.model';
@@ -6,16 +6,16 @@ import { Task } from './task.model';
 export class TaskPrototype {
     public id: number;
     public image: ImagePrototype;
-    public taskType: TaskType;
+    public taskGroup: TaskGroup;
     public user: User;
-    public active: boolean;
-    public completed: boolean;
-
+    public isComplete: boolean;
+    public isVisible: boolean;
+    public comment: string;
     constructor(task: Task) {
         this.id = task.id;
         this.image = task.image === undefined ? undefined : task.image.prototype();
         this.user = task.user;
-        this.active = task.active;
-        this.completed = task.completed;
+        this.isVisible = task.isVisible;
+        this.isComplete = task.isComplete;
     }
 }
