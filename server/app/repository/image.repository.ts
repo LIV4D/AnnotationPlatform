abbc2054a5ddca571c7eed138e51d5a37f4119b5
Monssaf Toukal <monsasf.toukal@polymtl.ca>
2019-05-23 17:24:08 -0400

    modification du image repo

--- server/app/repository/image.repository.ts
+++ server/app/repository/image.repository.ts
@@ -2,7 +2,7 @@ import { ConnectionProvider } from './connection.provider';
 import { injectable, inject } from 'inversify';
 import { Image } from '../models/image.model';
 import { ImageViewModel } from '../models/image.viewmodel';
-import { DeleteResult } from 'typeorm';
+import { DeleteResult, SelectQueryBuilder } from 'typeorm';
 
 @injectable()
 export class ImageRepository {
@@ -15,71 +15,54 @@ export class ImageRepository {
     }
 
     public async findAll(): Promise<Image[]> {
-        return await this.connectionProvider().then(connection =>
-            connection.getRepository(Image).find());
+        const connection = await this.connectionProvider();
+        return await connection.getRepository(Image).find();
     }
 
-    public async findAllWithCount(sort?: string, order?: string, page?: number, pageSize?: number, filters?: string):
-    Promise<ImageViewModel> {
-        if (!sort) {
-            sort = 'image.id';
-        }
-        if (!order) {
-            order = 'ASC';
-        }
-        if (!Number.isInteger(Number(page))) {
-            page = 0;
-        }
-        if (!Number.isInteger(Number(pageSize))) {
-            pageSize = 0;
-        }
-        return this.connectionProvider().then(connection => {
-            const qb = connection
-            .getRepository(Image)
+    public async findAllWithCount(sort: string = 'image.id',
+                                  order: string = 'ASC',
+                                  page: number = 0,
+                                  pageSize: number = 0,
+                                  filters?: string):
+        Promise<ImageViewModel> {
+        const connection = await this.connectionProvider();
+
+        const queryBuilder = connection.getRepository(Image)
             .createQueryBuilder('image')
-            .leftJoinAndSelect('image.imageType', 'imageType')
             .skip(page * pageSize)
             .take(pageSize);
-            // TypeORM doesn't accept a string for the order but only 'ASC' or 'DESC'
-            if (order.toUpperCase() === 'ASC') {
-                qb.orderBy(sort, 'ASC');
-            } else {
-                qb.orderBy(sort, 'DESC');
-            }
-            // Iterate through filters and add where conditions
-            if (filters) {
-                const filterArray = JSON.parse(filters);
-                let count = 0;
-                for (const key of Object.keys(filterArray)) {
-                    let imageColumn = '';
-                    // imageColumns are hard coded to avoid SQL injection
-                    const imageColumns = ['imageType.name', 'eye', 'hospital', 'patient', 'visit', 'code'];
-                    if (imageColumns.indexOf(key) > -1) {
-                        imageColumn = key;
-                    }
+        if (order.toUpperCase() === 'ASC') {
+            queryBuilder.orderBy(sort, 'ASC');
+        } else {
+            queryBuilder.orderBy(sort, 'DESC');
+        }
+        const queryResult = await this.filterImages(queryBuilder, filters);
+        const imageViewModel: ImageViewModel = { images: queryResult[0], count: queryResult[1] };
+        return imageViewModel;
+    }
 
-                    if (imageColumn !== '' && filterArray[key] && count === 0) {
-                        qb.where(`LOWER(${imageColumn}) like LOWER(:filter)`, { filter : `%${filterArray[key]}%` });
-                        count++;
-                    } else if (imageColumn !== '' && filterArray[key]) {
-                        const filterName = `filter${count}`;
-                        const filterNameObj = {};
-                        filterNameObj[filterName] = `%${filterArray[key]}%`;
-                        qb.andWhere(`LOWER(${imageColumn}) like LOWER(:${filterName})`, filterNameObj);
-                        count++;
-                    }
+    private async filterImages(queryBuilder: SelectQueryBuilder<Image>, filters?: string): Promise<[Image[], number]> {
+        if (filters) {
+            const filterJson = JSON.parse(filters);
+            let count = 0;
+            for (const key of Object.keys(filterJson)) {
+                // imageColumns are hard coded to avoid SQL injection
+                const imageColumns = ['imageType.name', 'eye', 'hospital', 'patient', 'visit', 'code'];
+                const imageColumnKey = imageColumns.indexOf(key) > -1 ? key : '';
+
+                if (imageColumnKey !== '' && filterJson[key] && count === 0) {
+                    queryBuilder.where(`LOWER(${imageColumnKey}) like LOWER(:filter)`, { filter: `%${filterJson[key]}%` });
+                    count++;
+                } else if (imageColumnKey !== '' && filterJson[key]) {
+                    const filterName = `filter${count}`;
+                    const filterNameObj = {};
+                    filterNameObj[filterName] = `%${filterJson[key]}%`;
+                    queryBuilder.andWhere(`LOWER(${imageColumnKey}) like LOWER(:${filterName})`, filterNameObj);
+                    count++;
                 }
             }
-            return qb.getManyAndCount();
-        })
-        .then(res => {
-            const vm: ImageViewModel = {
-                images: res[0],
-                count: res[1],
-            };
-            return vm;
-        });
-
+        }
+        return queryBuilder.getManyAndCount();
     }
 
     public async create(image: Image): Promise<Image> {
@@ -94,7 +77,7 @@ export class ImageRepository {
 
     public async find(id: number): Promise<Image> {
         return await this.connectionProvider().then(connection =>
-            connection.getRepository(Image).findOne({ where: { id }, relations : ['imageType'] }));
+            connection.getRepository(Image).findOne({ where: { id }, relations: ['imageType'] }));
     }
 
     public async findByPath(path: string): Promise<Image> {
@@ -104,7 +87,7 @@ export class ImageRepository {
 
     public async findWithForeignKeys(id: number): Promise<Image> {
         return await this.connectionProvider().then(connection =>
-            connection.getRepository(Image).findOne({ where: { id }, relations: ['preprocessings', 'revisions', 'tasks'] }));
+            connection.getRepository(Image).findOne({ where: { id }, relations: ['annotations', 'tasks'] }));
     }
 
     public async delete(image: Image): Promise<DeleteResult> {
