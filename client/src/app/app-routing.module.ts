50a358af7e86bb5b7cd6171905190087f8e4e885
v44ti <defaultinstantmail@gmail.com>
2020-02-06 18:38:39 -0500

    adding routes for the present components

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './login/login.component';
 
+import { LoginComponent } from './login/login.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { EditorComponent } from './editor/editor.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 const routes: Routes = [
-  { path: '', component: LoginComponent }
+  { path: '', component: LoginComponent },
+  { path: 'gallery', component: GalleryComponent },
+  { path: 'editor', component: EditorComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'tasks', component: TasksComponent }
 ];
 
 @NgModule({
