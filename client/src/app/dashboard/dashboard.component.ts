ff45f5848e57990399859e4ccb838edc341294bd
v44ti <defaultinstantmail@gmail.com>
2020-02-06 13:24:43 -0500

    corrected styleUrls' components path

--- client/src/app/dashboard/dashboard.component.ts
+++ client/src/app/dashboard/dashboard.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
 
--- client/src/app/gallery/gallery.component.ts
+++ client/src/app/gallery/gallery.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
-  styleUrls: ['./gallery.component.scss']
+  styleUrls: ['./gallery.component.css']
 })
 export class GalleryComponent implements OnInit {
 
