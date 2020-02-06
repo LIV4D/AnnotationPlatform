239e02c735b1333aab7f33cc095ecc84fee9f22c
v44ti <defaultinstantmail@gmail.com>
2020-02-06 13:19:43 -0500

    set up empty components: editor/gallery/tasks/dashboard

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,10 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginModule } from './login/login.module';
 import { SharedModule} from './shared/shared.module';
+import { EditorComponent } from './editor/editor.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
    declarations: [
       AppComponent,
+      EditorComponent,
+      GalleryComponent,
+      TasksComponent,
+      DashboardComponent,
    ],
    imports: [
       BrowserModule,
