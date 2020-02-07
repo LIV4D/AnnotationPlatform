3d7c80761e7130fbbaec2ad3b8aa782d0fc8cd22
cjameslok <christopher-james.lok@polymtl.ca>
2020-02-07 12:43:40 -0500

    JL added login html and scss but missing material

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -4,7 +4,7 @@ import { LoginFacadeService } from './login.facade.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
 
