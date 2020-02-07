17035e501a785d107523d1548c07b62596346d8c
v44ti <defaultinstantmail@gmail.com>
2020-02-07 01:13:46 -0500

    commenting NavBarService -- the service is not created yet

--- client/src/app/navigation-bar/navigation-bar.facade.service.ts
+++ client/src/app/navigation-bar/navigation-bar.facade.service.ts
@@ -4,14 +4,14 @@ import { LoginService } from '../shared/services/login.service';
 @Injectable()
 export class NavigationBarFacadeService {
 
-  /// NavBarService wrapper
-  private _navBarService: LoginService;
-  public get navBarService(): LoginService {
-    if (!this._navBarService) {
-      this._navBarService = this.injector.get(LoginService);
-    }
-    return this._navBarService;
-  }
+  /// NavBarService wrapper -- TODO: NavBarService is not created yet
+  // private _navBarService: NavBarService;
+  // public get navBarService(): NavBarService {
+  //   if (!this._navBarService) {
+  //     this._navBarService = this.injector.get(NavBarService);
+  //   }
+  //   return this._navBarService;
+  // }
   /// ... add the other service wrappers
 
   constructor(private injector: Injector) {  }
