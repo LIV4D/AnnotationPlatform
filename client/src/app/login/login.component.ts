57907d02066cd46a410bf3551380c1617cac5dd6
cjameslok <christopher-james.lok@polymtl.ca>
2020-02-08 15:37:04 -0500

    JL login page looks decent

--- client/src/app/login/login.component.ts
+++ client/src/app/login/login.component.ts
@@ -1,12 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginFacadeService } from './login.facade.service';
-
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+
+  public loginForm: FormGroup;
+  public email: string;
+  public password: string;
+
+  formErrors = {
+    email: '',
+    password: '',
+    server: ''
+  };
+
+  validationMessages = {
+    email: {
+        required: 'Email required'
+    },
+    password: {
+        required: 'Password required'
+    }
+  };
 
   isAuthenticated: boolean;
 
--- client/src/material/material.module.ts
+++ client/src/material/material.module.ts
@@ -1,13 +1,11 @@
 
 import { MatTooltipModule } from '@angular/material/tooltip';
-import { LayoutModule } from '@angular/cdk/layout';
 import { NgModule } from '@angular/core';
 
 @NgModule({
 
     exports: [
         MatTooltipModule,
-        LayoutModule,
     ]
 })
 
