a33f8e259a6faf92445ae3d10056011f214ab725
cjameslok <christopher-james.lok@polymtl.ca>
2020-02-08 16:12:27 -0500

    JL oops forgot the packages

--- client/src/material/material.module.ts
+++ client/src/material/material.module.ts
@@ -1,11 +1,14 @@
-
-import { MatTooltipModule } from '@angular/material/tooltip';
 import { NgModule } from '@angular/core';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { LayoutModule } from '@angular/cdk/layout';
+
+
 
 @NgModule({
 
     exports: [
         MatTooltipModule,
+        LayoutModule
     ]
 })
 
