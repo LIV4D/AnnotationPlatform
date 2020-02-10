e43ccc13f1013f2e2fcf18fe5f1f463e0f59848f
Gab Fortin <gabfortin@icloud.com>
2020-02-10 11:36:15 -0500

    Added login methods

--- client/src/app/shared/services/login.service.ts
+++ client/src/app/shared/services/login.service.ts
@@ -1,11 +1,60 @@
+import { catchError, map } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+import { HttpErrorResponse, HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable()
 export class LoginService {
 
-  constructor() { }
+  // tslint:disable-next-line:variable-name
+  private _user: any;
+  // tslint:disable-next-line:variable-name
+  private _name: string;
 
-  isAuthenticated() {
+  constructor(private http: HttpClient) { }
+
+  get email(): string {
+    return this._user ? this._user.user.email : null;
+  }
+
+  get name(): string {
+    return this._user ? this._user.user.name : null;
+  }
+
+  login(email: string, password: string): Observable<any> {
+    this._user = null;
+    const pw = password;
+    return this.http.post<any>('/auth/login', { username: email, password: pw }).
+      pipe(
+        map((user: any) => {
+          // login successful if there's a jwt token in the response
+          if (user && user.token) {
+            // store user details and jwt token in local storage to keep user logged in between page refreshes
+            localStorage.setItem('currentUser', JSON.stringify(user));
+          }
+          this._user = user;
+          return user;
+        }));
+  }
+
+  isAuthenticated(): boolean {
+    const userStr = localStorage.getItem('currentUser');
+    if (!userStr) {
+      return false;
+    }
+    if (!this._user) {
+      this._user = JSON.parse(localStorage.getItem('currentUser'));
+    }
     return true;
   }
+
+  logout(): void {
+    // remove user from local storage to log user out
+    localStorage.removeItem('currentUser');
+    this._user = null;
+  }
+
+  // isAuthenticated() {
+  //   return true;
+  // }
 }
