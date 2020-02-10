4770f70c17b8a8d47cfcd844671bce151d1179ae
cjameslok <christopher-james.lok@polymtl.ca>
2020-02-10 13:59:44 -0500

    JL fix app module ts

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,15 +1,25 @@
+// Modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+
+// Components
 import { AppComponent } from './app.component';
 import { EditorComponent } from './editor/editor.component';
 import { GalleryComponent } from './gallery/gallery.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
+
+// Services
 import { LoginFacadeService } from './login/login.facade.service';
 import { LoginService } from './shared/services/login.service';
+
+// Material
+import { MaterialModule } from '../material/material.module';
+// import { MatIconModule } from "@angular/material/icon";
+
+
 
 @NgModule({
    declarations: [
@@ -23,6 +33,7 @@ import { LoginService } from './shared/services/login.service';
    imports: [
       BrowserModule,
       AppRoutingModule,
+      MaterialModule
    ],
    providers: [
       LoginFacadeService,
