05073b602ac25274ad1f38f847ba0d6d2543965e
Nuno <nuno.silvapinto@outlook.com>
2020-02-21 21:35:59 -0500

    [ Tasks List ] REFACTOR HeaderService

--- client/src/app/shared/services/header.service.ts
+++ client/src/app/shared/services/header.service.ts
@@ -1,49 +1,65 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { HttpEventType, HttpResponse } from '@angular/common/http';
 import {map, filter} from 'rxjs/operators';
+import { HttpEventType, HttpResponse } from '@angular/common/http';
 
 @Injectable()
 export class HeaderService {
 
+  progress: number;
+  isShown: boolean;
+  name?: string;
+  isDownloading?: boolean;
 
-    progress: number;
-    show: boolean;
-    name?: string;
-    download?: boolean;
+  constructor() {}
 
 
+  /**
+   * show progress
+   * @param showned: a progress bar can be showned or hidden
+   * @param [Name]: name of the progress bar
+   * @param [Download]: tells if the progress bar is downloading, True by default
+   */
+  cbShowProgress(isShown: boolean, Name?: string, isDownloading?: boolean) {
+    this.isShown = isShown;
+    this.name = Name;
+    this.isDownloading = isDownloading;
+  }
 
-    constructor() {
-    }
 
-    cbShowProgress(Show: boolean, Name?: string, Download?: boolean) {
-        this.show = Show;
-        this.name = Name;
-        this.download = Download;
-    }
+  /**
+   * progress
+   * @param Progress: progression number of the bar
+   */
+  cbProgress(Progress: number) {
+    this.progress = Progress;
+  }
 
-    cbProgress(Progress: number) {
-        this.progress = Progress;
-    }
 
-    display_progress(request: Observable<any>, name: string, download= true): Observable<any> {
-        this.cbShowProgress(true, name, download);
-        return request.pipe(filter(res => {
-            if (res.type === HttpEventType.DownloadProgress) {
-                if (this.cbProgress) {
-                    this.cbProgress(res.loaded / res.total);
-                }
-            } else if (res.type === HttpEventType.UploadProgress) {
-                if (this.cbProgress) {
-                    this.cbProgress(res.loaded / res.total);
-                }
-            } else if (res.type === HttpEventType.Response) {
-                this.cbShowProgress(false);
-                return true;
-            }
-            return false;
-        }), map(res => (res as HttpResponse<any>).body));
-    }
+  /**
+   * Displays progress: Display progress of a loading of datas in a progress bar
+   * @param request: Observable<any> emiting datas
+   * @param name: name of the progress bar
+   * @param [download] : tells if the progress bar is downloading, True by default
+   * @returns progress : Observable<any> response from the request
+   */
+  display_progress(request: Observable<any>, name: string, isDownloading= true): Observable<any> {
+      this.cbShowProgress(true, name, isDownloading);
+      return request.pipe(filter(res => {
+          if (res.type === HttpEventType.DownloadProgress) {
+              if (this.cbProgress) {
+                  this.cbProgress(res.loaded / res.total);
+              }
+          } else if (res.type === HttpEventType.UploadProgress) {
+              if (this.cbProgress) {
+                  this.cbProgress(res.loaded / res.total);
+              }
+          } else if (res.type === HttpEventType.Response) {
+              this.cbShowProgress(false);
+              return true;
+          }
+          return false;
+      }), map(res => (res as HttpResponse<any>).body));
+  }
 
 }
