829d3af3936d15a21aaefa3e8c075a65e921b18a
Gab Fortin <gabfortin@icloud.com>
2020-02-24 11:49:01 -0500

    Added toolService to toolbox.service and tool.model

--- client/src/app/shared/models/tool.model.ts
+++ client/src/app/shared/models/tool.model.ts
@@ -1,14 +1,31 @@
+import { LayersService } from './../services/Editor/layers.service';
+import { EditorService } from './../services/Editor/editor.service';
+// import { Tool } from './../services/Editor/editor.service';
+// import { EditorService } from './../services/Editor/editor.service';
+
+export interface ToolServices {
+  layersService: LayersService;
+  editorService: EditorService;
+  // toolPropertiesService: ToolPropertiesService;
+  // biomarkersService: BiomarkersService;
+}
+
 export class Tool {
   disabled: boolean;
+
+  protected layersService: LayersService;
+  protected editorService: EditorService;
 
   public name: string;
   public imagePath: string;
   public tooltip: string;
 
-  // constructor(private _name, private _iconPath: string, private _tooltip: string, toolServices: ToolServices) {
-  constructor(name: string, imagePath: string, tooltip: string) {
+  constructor(name: string, imagePath: string, tooltip: string, toolServices: ToolServices) {
+  // constructor(name: string, imagePath: string, tooltip: string) {
     this.name = name;
     this.imagePath = imagePath;
     this.tooltip = tooltip;
+    this.layersService = toolServices.layersService;
+    this.editorService = toolServices.editorService;
   }
 }
