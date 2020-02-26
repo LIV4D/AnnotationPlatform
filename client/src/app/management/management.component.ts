767265d48da4a3c1a6bef1673d6e5fd0248351d9
Mitchell Mastromonaco <mitchell.mastromonaco@gmail.com>
2020-02-26 18:29:15 +0000

    Added personal reminders for management. Changed null value for lastModifiedTime.

--- client/src/app/management/management.component.ts
+++ client/src/app/management/management.component.ts
@@ -11,5 +11,8 @@ export class ManagementComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  // ok so, need a function to load a model.
+  // need a function to put that model into text fields.
+  // so for each attribute of the interface, create a text field.
+  // need a function to send the text fields chosen to the database so it can be created.
 }
