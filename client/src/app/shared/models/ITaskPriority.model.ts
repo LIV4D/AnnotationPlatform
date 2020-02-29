443f784a981fdd14aefa61bc7c5ca52aadb0a69c
cjameslok <christopher-james.lok@polymtl.ca>
2020-02-28 21:58:39 -0500

    taskpriority model

--- client/src/app/shared/models/ITaskPriority.model.ts
+++
@@ -1,5 +0,0 @@
-export class ITaskPriority {
-    public userId = 0;
-    public taskId = 0;
-    public priority = 0;
-}
--- client/src/app/shared/models/taskPriority.model.ts
+++ client/src/app/shared/models/taskPriority.model.ts
@@ -1,3 +1,5 @@
 export class TaskPriority {
-
+    public userId = 0;
+    public taskId = 0;
+    public priority = 0;
 }
