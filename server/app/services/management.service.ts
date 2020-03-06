045c3c635baef0c961f0dd8d9e9ba131faa35da2
Mitchell Mastromonaco <mitchell.mastromonaco@gmail.com>
2020-03-06 21:57:38 +0000

    Invisible files now removed

--- server/app/services/management.service.ts
+++ server/app/services/management.service.ts
@@ -1,14 +1,17 @@
 import { injectable } from 'inversify';
 import fs = require('fs');
+import path = require('path');
 
 @injectable()
 export class ManagementService {
-    private readonly modelDirectoryName = '../models/';
+    private modelDirectoryName = '../models/';
 
     public async listAllModels(): Promise<string[]> {
-        const filesWithExtensions = await fs.readdirSync(this.modelDirectoryName).map(file => {
+        this.modelDirectoryName = path.resolve(__dirname, this.modelDirectoryName);
+        let filesWithExtensions = await fs.readdirSync(this.modelDirectoryName).map(file => {
             return file;
         });
+        filesWithExtensions = filesWithExtensions.filter( (file) => !file.match(/(.map)$/));
         return filesWithExtensions.map(fileWithExtensions => {
             return fileWithExtensions.replace(/(\.[^/.]+)+$/, '');
         });
