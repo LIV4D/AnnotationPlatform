7fb1a46947423705cacd49c398330f4a6592eaae
cjameslok <christopher-james.lok@polymtl.ca>
2020-03-07 13:26:58 -0500

    message when bundle is assigned

--- client/src/app/tasks/tasks-bundles/tasks-bundles.component.ts
+++ client/src/app/tasks/tasks-bundles/tasks-bundles.component.ts
@@ -25,6 +25,8 @@ export class TasksBundlesComponent implements OnInit {
     tertiaryTaskTypeDescription: 'Description',
   } ;
 
+  assignedBundle: number;
+  isBundleAssigned = false;
   noData: boolean;
 
   constructor(private facadeService: TasksBundlesFacadeService) {
@@ -45,17 +47,20 @@ export class TasksBundlesComponent implements OnInit {
           isNullOrUndefined(this.bundles.tertiaryBundle) || this.bundles.tertiaryBundle.length === 0);
   }
 
-  async assignBundleTasks(tasks: ITasks[]) {
+  async assignBundleTasks(tasks: ITasks[], bundleNumber: number) {
     const taskIds = [];
     tasks.forEach(task => {
       taskIds.push(task.id);
     });
     const res = await this.facadeService.assignBundleTasks(taskIds);
-    console.log(res);
+
+    // check server response
     if (res === 204) {
+      this.assignedBundle = bundleNumber;
+      this.isBundleAssigned = true;
       this.loadBundles();
     } else {
-      console.log("There was error while assigning the task bundle");
+      console.log('There was error while assigning the task bundle');
     }
   }
 
