0c35c8845690ed6f0a5e480923bab3467a1efaeb
Mitchell Mastromonaco <mitchell.mastromonaco@gmail.com>
2020-03-12 18:15:06 +0000

    Changed readdir so that it is async

--- server/app/services/management.service.ts
+++ server/app/services/management.service.ts
@@ -1,6 +1,7 @@
 import { injectable } from 'inversify';
 import fs = require('fs');
 import path = require('path');
+import util = require('util');
 
 @injectable()
 export class ManagementService {
@@ -8,9 +9,10 @@ export class ManagementService {
 
     public async listAllModels(): Promise<string[]> {
         this.modelDirectoryName = path.resolve(__dirname, this.modelDirectoryName);
-        let filesWithExtensions = await fs.readdirSync(this.modelDirectoryName).map(file => {
-            return file;
-        });
+
+        const readdir = util.promisify(fs.readdir);
+        let filesWithExtensions = await readdir(this.modelDirectoryName);
+
         filesWithExtensions = filesWithExtensions.filter( (file) => !file.match(/(.map)$/));
         return filesWithExtensions.map(fileWithExtensions => {
             return fileWithExtensions.replace(/(\.[^/.]+)+$/, '');
