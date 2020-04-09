0d50c1557d79e4807d5c85b605b467c081c93b5d
Nuno <nuno.silvapinto@outlook.com>
2020-04-09 19:12:20 -0400

    modified empty revision

--- server/app/controllers/annotation.controller.ts
+++ server/app/controllers/annotation.controller.ts
@@ -11,39 +11,39 @@ const EMPTY_REVISION: any = {
     biomarkers: [
         {type: 'Lesions',
          biomarkers: [
-             { type: 'Others', color: '#fff4ee', },
-             { type: 'Pigmented lesions', color: '#5bffb7' },
+             { type: 'Others', color: '#fff4ee', dataImage:'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=' },
+             { type: 'Pigmented lesions', color: '#5bffb7', dataImage:'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=' },
              {type: 'Bright',
               biomarkers: [
-                  { type: 'Cotton Wool Spots', color: '#7a9d32' },
-                  { type: 'Drusen', color: '#3cb371' },
-                  { type: 'Exudates', color: '#85ffa6' },
-                  { type: 'Uncertain - Bright', color: '#a9ff84' }
+                  { type: 'Cotton Wool Spots', color: '#7a9d32', dataImage:'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=' },
+                  { type: 'Drusen', color: '#3cb371', dataImage:'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=' },
+                  { type: 'Exudates', color: '#85ffa6', dataImage:'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=' },
+                  { type: 'Uncertain - Bright', color: '#a9ff84', dataImage:'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=' }
               ]},
              {type: 'Red',
               biomarkers: [
-                  { type: 'Hemorrhages', color: '#4b18ff' },
-                  { type: 'Microaneurysms', color: '#2a63fd' },
-                  { type: 'Sub-retinal hemorrhage', color: '#7a2afd' },
-                  { type: 'Pre-retinal hemorrhage', color: '#a12afd' },
-                  { type: 'Neovascularization', color: '#ba2afd' },
-                  { type: 'Uncertain - Red', color: '#d62afd' }
+                  { type: 'Hemorrhages', color: '#4b18ff', dataImage:'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=' },
+                  { type: 'Microaneurysms', color: '#2a63fd', dataImage:'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=' },
+                  { type: 'Sub-retinal hemorrhage', color: '#7a2afd', dataImage:'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=' },
+                  { type: 'Pre-retinal hemorrhage', color: '#a12afd', dataImage:'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=' },
+                  { type: 'Neovascularization', color: '#ba2afd', dataImage:'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=' },
+                  { type: 'Uncertain - Red', color: '#d62afd', dataImage:'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=' }
               ]}
          ]
         },
         {type: 'Normal',
          biomarkers: [
-             { type: 'Macula', color: '#be3c1b' },
+             { type: 'Macula', color: '#be3c1b', dataImage:'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=' },
              {type: 'Optic Nerve',
               biomarkers: [
-                  { type: 'Disk', color: '#ddc81c' },
-                  { type: 'Cup', color: '#dda61c' }
+                  { type: 'Disk', color: '#ddc81c', dataImage:'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=' },
+                  { type: 'Cup', color: '#dda61c', dataImage:'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=' }
               ]},
              {type: 'Vasculature',
               biomarkers: [
-                  { type: 'Arteries', color: '#dd1c1c' },
-                  { type: 'Veins', color: '#6d13b2' },
-                  { type: 'Vessels', color: '#770067' }
+                  { type: 'Arteries', color: '#dd1c1c', dataImage:'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=' },
+                  { type: 'Veins', color: '#6d13b2', dataImage:'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=' },
+                  { type: 'Vessels', color: '#770067', dataImage:'data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=' }
               ]},
          ],
         }
