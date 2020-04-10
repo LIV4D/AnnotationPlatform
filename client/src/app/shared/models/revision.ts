566be7c5aa6de6cf49baae279c4e3cf3eed7bec5
Mitchell Mastromonaco <mitchell.mastromonaco@gmail.com>
2020-04-09 22:21:06 -0400

    Added recursive function into separate service in order to liberate model.

--- client/src/app/shared/models/revision.ts
+++ client/src/app/shared/models/revision.ts
@@ -2,31 +2,7 @@ import { isNullOrUndefined } from 'util';
 
 export class Revision {
     biomarkers?: Array<Revision>;
-    type: string;
+    type?: string;
     color?: string;
-    dataImage?: Buffer; // ou string
-
-    public setDataImageRecursive(type: string, dataImage: Buffer) {
-        if (this.type === type) {
-            this.dataImage = dataImage;
-        } else {
-            if (!isNullOrUndefined(this.biomarkers) && this.biomarkers.length > 0) {
-                this.setDataImageRecursive(type, dataImage);
-            }
-        }
-    }
-    /**
-     * 
-     * @param typeDataMap map consisting of each type (from Revision class) associated with its equivalent dataImage (from Revision class)
-     */
-    public setDataImageRecursiveWithMap(typeDataMap: Map<string, Buffer>) {
-        if (typeDataMap.has(this.type)) {
-            this.dataImage = typeDataMap.get(this.type);
-            typeDataMap.delete(this.type);
-        }
-
-        if (typeDataMap.size > 0){
-            this.setDataImageRecursiveWithMap(typeDataMap);
-        }
-    }
+    dataImage?: string; // ou string
 }
