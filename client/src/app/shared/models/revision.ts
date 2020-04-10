87782757db0b52bc47d5db89484c19779fc98538
Mitchell Mastromonaco <mitchell.mastromonaco@gmail.com>
2020-04-09 21:40:24 -0400

    Recursive function using map

--- client/src/app/shared/models/revision.ts
+++ client/src/app/shared/models/revision.ts
@@ -15,4 +15,18 @@ export class Revision {
             }
         }
     }
+    /**
+     * 
+     * @param typeDataMap map consisting of each type (from Revision class) associated with its equivalent dataImage (from Revision class)
+     */
+    public setDataImageRecursiveWithMap(typeDataMap: Map<string, Buffer>) {
+        if (typeDataMap.has(this.type)) {
+            this.dataImage = typeDataMap.get(this.type);
+            typeDataMap.delete(this.type);
+        }
+
+        if (typeDataMap.size > 0){
+            this.setDataImageRecursiveWithMap(typeDataMap);
+        }
+    }
 }
