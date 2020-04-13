84442d7038ceedefe2fb440a326c10ffb5a46d39
v44ti <defaultinstantmail@gmail.com>
2020-04-13 19:09:50 -0400

    changing comment box structure:

--- client/src/app/shared/services/Editor/comment-box.service.ts
+++ client/src/app/shared/services/Editor/comment-box.service.ts
@@ -1,21 +1,45 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { Subject } from 'rxjs';
+import { Tool } from './Tools/tool.service';
+import { Point } from './Tools/point.service';
+import { CanvasDimensionService } from './canvas-dimension.service';
+import { LayersService } from './layers.service';
+import { BiomarkerService } from './biomarker.service';
 
-@Injectable({
-  providedIn: 'root',
-})
-export class CommentBoxService {
+// @Injectable({
+//   providedIn: 'root',
+// })
+export class CommentBoxService extends Tool {
 
   commentBoxCheckBoxClicked: Subject<boolean>;
+  private position: Point;
 
-  constructor() {
+  // constructor() {
+  //   this.commentBoxCheckBoxClicked = new Subject<any>();
+  // }
+
+  constructor(name: string, iconPath: string, tooltip: string, canvasDimensionService: CanvasDimensionService, layersService: LayersService, biomarkerService: BiomarkerService) {
+    super(name, iconPath, tooltip, canvasDimensionService, layersService);
+
     this.commentBoxCheckBoxClicked = new Subject<any>();
   }
 
-  sendStateCommentBox(checkBoxCommentBox: boolean) {
-    this.commentBoxCheckBoxClicked.next(checkBoxCommentBox);
+  onCursorDown(point: Point): void {
+    this.position = point;
+    const currentBiomarker = this.layersService.getCurrentBiomarkerCanvas();
+    if (currentBiomarker) {
+
+    }
   }
 
- 
+  onCursorUp(): void {
+    console.log('%c ONCURSOR_UP: ', 'color:black;background:yellow;');
+    const currentBiomarker = this.layersService.getCurrentBiomarkerCanvas();
+  }
+
+  sendStateCommentBox(checkBoxCommentBox: boolean) {
+    console.log('%c sendStateCommentBox: ', 'color:black;background:yellow;');
+    this.commentBoxCheckBoxClicked.next(checkBoxCommentBox);
+  }
 }
